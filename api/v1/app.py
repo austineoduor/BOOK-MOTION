46cbc04b82f6ab14077aa0bbba396d7e1ba63f7c
austineoduor <austineoduor57@gmail.com>
2023-03-21 21:42:14 +0300

    files

--- api/v1/app.py
+++ api/v1/app.py
@@ -31,7 +31,7 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 app.config['SWAGGER'] = {
-    'title': 'LIBRARIFY Restful API',
+    'title': 'BOOKMOTION Restful API',
     'uiversion': 3
 }
 
@@ -40,8 +40,8 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('LIBRARIFY_API_HOST')
-    port = environ.get('LIBRARIFY_API_PORT')
+    host = environ.get('HOST')
+    port = environ.get('PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
