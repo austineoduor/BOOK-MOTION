96b9b3b0ed002c39aa27227ae0f9d1a3bb84e23e
Mmaureeny <muthonimaureen4@outlook.com>
2023-03-23 08:29:28 +0000

    static files

--- frontend/server.js
+++ frontend/server.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'). // Include ExpressJS
+const app = express(). // Create an ExpressJS app
+const bodyParser = require('body-parser'); // Middleware
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Route for the sign-up page
-app.get('/sign', (req, res) => {
-  res.sendFile(__dirname + '/public/sign.html');
+// Route to Homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/index.html');
 });
 
-// Route for the sign-in page
-app.get('/signup', (req, res) => {
-  res.sendFile(__dirname + '/public/signup.html');
+// Route to Signin-signup Page
+app.get('/signin', (req, res) => {
+  res.sendFile(__dirname + '/static/signin-signup.html');
 });
 
-// Route for the home page
-app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/public/home.html');
+app.post('/signin', (req, res) => {
+  // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
 });
+
+const port = 3000 // Port we will listen on
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
-
